6ff5bc7915964120d3063df703f726e2bce5a6a0
Matthew Partin <mdpxboss@yahoo.com>
2021-11-03 09:04:13 -0400

    School Catalogue project finished

--- SchoolCatalogue.js
+++ SchoolCatalogue.js
@@ -1,41 +1,74 @@
-// Parent Class School
+// Parent 1 Class School
 class School {
-    constructor(name, level, numberOfStudents) {
-      this._name = name;
-      this._level = level;
-      this._numberOfStudents = 0;
-    }
-  
-    get name() {
-      return this._name;
-    }
-  
-    get level() {
-      if (level != 'primary' || 'middle' || 'high') {
-        console.log('Please input an appropriate level. (Primary, Middle,  or High)') 
-      } else {
-        return this._level;
-      }
-    }
-  
-    get numberOfStudents() {
-      return this._numberOfStudents;
-    }
-  
-    set numberOfStudents(newNumberOfStudents) {
-      if (typeof newNumberOfStudents !== number) {
-        console.log('Error! Please enter valid number.')
-      } else {
-      this._numberOfStudents = newNumberOfStudents;
-      }
-    }
-  
-    quickFacts() {
-      console.log(`${name} educates ${numberOfStudents} at the ${level} school level.`);
-    }
-  
-    static pickSubstituteTeacher(substituteTeachers) {
-      const randNum = Math.random(Math.floor() * substituteTeachers.length)
-      return substituteTeachers[randNum];
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get level() {
+      return this._level;
+  }
+
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(newNumberOfStudents) {
+    if (typeof newNumberOfStudents !== number) {
+      console.log('Error! Please enter valid number.')
+    } else {
+    this._numberOfStudents = newNumberOfStudents;
     }
   }
+
+  quickFacts() {
+    console.log(`${this.name} educates ${this.numberOfStudents} at the ${this.level} school level.`);
+  }
+
+  static pickSubstituteTeacher(substituteTeachers) {
+    const randNum = Math.random(Math.floor() * substituteTeachers.length)
+    return substituteTeachers[randNum];
+  }
+}
+
+// Child 1 Primary School 
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, 'primary', numberOfStudents)
+    this._pickupPolicy = pickupPolicy;
+  }
+
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
+}
+
+// Child 2 High School 
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, 'high', numberOfStudents)
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams() {
+    return this._sportsTeams;
+    console.log(sportsTeams);
+  }
+}
+
+
+
+// Instances (Testing)
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.quickFacts();
+
+
+
